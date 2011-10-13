4dbc3b803636e8a626b213eb4aaae333c7f9389c
Ian Young <ian.greenleaf@gmail.com>
2011-10-12 23:22:13 -0700

    Hooray, got rid of jquery hacks

--- github_autocomplete.user.js
+++ github_autocomplete.user.js
@@ -2,18 +2,17 @@
 // @name           Github autocomplete
 // @namespace      iangreenleaf.com
 // @include        https://github.com/*
+// @require        http://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js
 // ==/UserScript==
 
-(function(unsafeWindow, $){
-  try {
-    var GM_log = function(obj) { unsafeWindow.console.log(obj); }
-
-    // Assume jQuery because GitHub uses jquery
-    $(".comment-form").keyup(function(ev) {
-      GM_log("keypress");
-    });
-
-  } catch(e) {
-    GM_log(e);
-  }
-})(unsafeWindow, unsafeWindow.jQuery);
+$(".comment-form").keyup(function(event) {
+  var lastWord = $(event.target).val().split(/ |\n/).pop();
+  if (lastWord[0] != "@" || lastWord.length < 5) return;
+  var search = lastWord.slice(1);
+  $.get(
+    "https://github.com/api/v2/json/user/search/" + search,
+    function(data) {
+      console.log(data);
+    }
+    );
+});
