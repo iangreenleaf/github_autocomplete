7d8e70b1fa76e9e1e7fdf8bb85a02570412b67b1
Ian Young <ian.greenleaf@gmail.com>
2011-10-14 21:27:04 -0700

    Show autocomplete results below form

--- github_autocomplete.user.js
+++ github_autocomplete.user.js
@@ -5,19 +5,32 @@
 // @require        http://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js
 // ==/UserScript==
 
+var getAutocompleteResults = function() {
+  var results = $("#user_autocomplete_results");
+  if (results.length == 0) {
+    results = $("<ul id='user_autocomplete_results'></ul>");
+  }
+  return results;
+}
+
 $(".comment-form").keyup(function(event) {
   var lastWord = $(event.target).val().split(/ |\n/).pop();
-  if (lastWord[0] != "@" || lastWord.length < 5) return;
+  if (lastWord[0] != "@" || lastWord.length < 5) {
+      getAutocompleteResults().empty();
+      return;
+  }
   var search = lastWord.slice(1);
   $.get(
     "https://github.com/autocomplete/users?q=" + search + "&limit=10",
     function(data) {
+      var box = getAutocompleteResults();
+      box.empty();
       $.each(data.split("\n"), function(i,str) {
         username = str.split(" ")[0];
         gravatar = str.split(" ")[1];
-        console.log(username);
-        console.log(gravatar);
+        box.append($("<li>" + username + "</li>"));
       });
+      $(".comment-form").after(box);
     }
   );
 });
