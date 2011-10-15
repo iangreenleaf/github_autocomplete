bb52ac0fba24ac5b37c31c7c841386429ec27e41
Ian Young <ian.greenleaf@gmail.com>
2011-10-14 23:34:45 -0700

    Revert "Load jQuery on Chrome"
    This reverts commit bd0e5403a041faa676bc0e3477bc499279fb9226.

--- github_autocomplete.user.js
+++ github_autocomplete.user.js
@@ -7,88 +7,73 @@
 // @require        http://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js
 // ==/UserScript==
 
-var main = function() {
-  var getAutocompleteResults = function(forTextField) {
-    var results = $("#user_autocomplete_results");
-    if (results.length == 0) {
-      results = $("<ul id='user_autocomplete_results' class='ac_results'></ul>");
-    }
-    results.data("activeFormId", forTextField.prop("id"));
-    return results;
+var getAutocompleteResults = function(forTextField) {
+  var results = $("#user_autocomplete_results");
+  if (results.length == 0) {
+    results = $("<ul id='user_autocomplete_results' class='ac_results'></ul>");
   }
-
-  var autocompleteSelected = function(username, sender) {
-    var textfield = $("#" + sender.data("activeFormId"));
-    var text = textfield.val();
-    var start = text.lastIndexOf("@");
-    textfield.val(text.substr(0, start + 1) + username + " ");
-  }
-
-  $("#user_autocomplete_results li").live("click", function() {
-    var username = $(this).text();
-    var box = $(this).parent();
-    autocompleteSelected(username, box);
-    box.empty();
-  });
-
-  $("#user_autocomplete_results li").live("mouseenter", function() { $(this).addClass("ac_over"); });
-  $("#user_autocomplete_results li").live("mouseleave", function() { $(this).removeClass("ac_over"); });
-
-  $("#user_autocomplete_results").live("mouseenter", function() { $($("li", this).first()).removeClass("ac_over"); });
-  $("#user_autocomplete_results").live("mouseleave", function() { $($("li", this).first()).addClass("ac_over"); });
-
-  $(".comment-form textarea").keydown(function(e) {
-    var activeField = $(this);
-    var box = getAutocompleteResults(activeField);
-
-    if (
-      (e.keyCode == 13 || e.keyCode == 9)
-      && (items = $("li", box)).length > 0
-    ) {
-      e.preventDefault();
-      autocompleteSelected($(items.first()).text(), box);
-      return false;
-    }
-  });
-
-  $(".comment-form textarea").keyup(function(e) {
-    var activeField = $(this);
-    var box = getAutocompleteResults(activeField);
-
-    var lastWord = $(e.target).val().split(/ |\n/).pop();
-    if (lastWord[0] != "@" || lastWord.length < 3) {
-        box.empty();
-        return;
-    }
-
-    var search = lastWord.slice(1);
-    $.get(
-      "https://github.com/autocomplete/users?q=" + search + "&limit=10",
-      function(data) {
-        box.empty();
-        $.each(data.split("\n"), function(i,str) {
-          username = str.split(" ")[0];
-          gravatar = str.split(" ")[1];
-          var item = $("<li></li>").text(username);
-          item.prepend($("<img src='https://secure.gravatar.com/avatar/" + gravatar + "?s=140&d=https%3A%2F%2Fgithub.com%2Fimages%2Fgravatars%2Fgravatar-140.png' />").prop("width", 20).prop("height", 20));
-          box.append(item);
-        });
-        activeField.after(box);
-        box.trigger("mouseleave");
-      }
-    );
-  });
+  results.data("activeFormId", forTextField.prop("id"));
+  return results;
 }
 
-// Load jQuery for Chrome (thanks to Erik Vold, http://r.evold.ca/jquery4us)
-var addJQuery = function(callback) {
-  var script = document.createElement("script");
-  script.setAttribute("src", "http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js");
-  script.addEventListener('load', function() {
-    var script = document.createElement("script");
-    script.textContent = "(" + callback.toString() + ")();";
-    document.body.appendChild(script);
-  }, false);
-  document.body.appendChild(script);
+var autocompleteSelected = function(username, sender) {
+  var textfield = $("#" + sender.data("activeFormId"));
+  var text = textfield.val();
+  var start = text.lastIndexOf("@");
+  textfield.val(text.substr(0, start + 1) + username + " ");
 }
-addJQuery(main);
+
+$("#user_autocomplete_results li").live("click", function() {
+  var username = $(this).text();
+  var box = $(this).parent();
+  autocompleteSelected(username, box);
+  box.empty();
+});
+
+$("#user_autocomplete_results li").live("mouseenter", function() { $(this).addClass("ac_over"); });
+$("#user_autocomplete_results li").live("mouseleave", function() { $(this).removeClass("ac_over"); });
+
+$("#user_autocomplete_results").live("mouseenter", function() { $($("li", this).first()).removeClass("ac_over"); });
+$("#user_autocomplete_results").live("mouseleave", function() { $($("li", this).first()).addClass("ac_over"); });
+
+$(".comment-form textarea").keydown(function(e) {
+  var activeField = $(this);
+  var box = getAutocompleteResults(activeField);
+
+  if (
+    (e.keyCode == 13 || e.keyCode == 9)
+    && (items = $("li", box)).length > 0
+  ) {
+    e.preventDefault();
+    autocompleteSelected($(items.first()).text(), box);
+    return false;
+  }
+});
+
+$(".comment-form textarea").keyup(function(e) {
+  var activeField = $(this);
+  var box = getAutocompleteResults(activeField);
+
+  var lastWord = $(e.target).val().split(/ |\n/).pop();
+  if (lastWord[0] != "@" || lastWord.length < 3) {
+      box.empty();
+      return;
+  }
+
+  var search = lastWord.slice(1);
+  $.get(
+    "https://github.com/autocomplete/users?q=" + search + "&limit=10",
+    function(data) {
+      box.empty();
+      $.each(data.split("\n"), function(i,str) {
+        username = str.split(" ")[0];
+        gravatar = str.split(" ")[1];
+        var item = $("<li></li>").text(username);
+        item.prepend($("<img src='https://secure.gravatar.com/avatar/" + gravatar + "?s=140&d=https%3A%2F%2Fgithub.com%2Fimages%2Fgravatars%2Fgravatar-140.png' />").prop("width", 20).prop("height", 20));
+        box.append(item);
+      });
+      activeField.after(box);
+      box.trigger("mouseleave");
+    }
+  );
+});
