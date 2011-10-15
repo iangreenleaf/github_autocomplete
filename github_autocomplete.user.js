f89e3429fdee7ff67b456602915d2c58f7ea1191
Ian Young <ian.greenleaf@gmail.com>
2011-10-14 21:52:50 -0700

    Styling, images, click handler

--- github_autocomplete.user.js
+++ github_autocomplete.user.js
@@ -5,33 +5,42 @@
 // @require        http://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js
 // ==/UserScript==
 
-var getAutocompleteResults = function() {
+var getAutocompleteResults = function(forTextField) {
   var results = $("#user_autocomplete_results");
   if (results.length == 0) {
-    results = $("<ul id='user_autocomplete_results'></ul>");
+    results = $("<ul id='user_autocomplete_results' class='ac_results'></ul>");
   }
+  results.data("activeFormId", forTextField.prop("id"));
   return results;
 }
 
-$(".comment-form").keyup(function(event) {
-  var activeForm = $(this);
+$("#user_autocomplete_results li").live("click", function() {
+  var username = $(this).text();
+  console.log(username);
+  console.log($(this).parent().data("activeFormId"));
+});
+
+$(".comment-form textarea").keyup(function(event) {
+  var activeField = $(this);
   var lastWord = $(event.target).val().split(/ |\n/).pop();
   if (lastWord[0] != "@" || lastWord.length < 4) {
-      getAutocompleteResults().empty();
+      getAutocompleteResults(activeField).empty();
       return;
   }
   var search = lastWord.slice(1);
   $.get(
     "https://github.com/autocomplete/users?q=" + search + "&limit=10",
     function(data) {
-      var box = getAutocompleteResults();
+      var box = getAutocompleteResults(activeField);
       box.empty();
       $.each(data.split("\n"), function(i,str) {
         username = str.split(" ")[0];
         gravatar = str.split(" ")[1];
-        box.append($("<li>" + username + "</li>"));
+        var item = $("<li></li>").text(username);
+        item.prepend($("<img src='https://secure.gravatar.com/avatar/" + gravatar + "?s=140&d=https%3A%2F%2Fgithub.com%2Fimages%2Fgravatars%2Fgravatar-140.png' />").prop("width", 20).prop("height", 20));
+        box.append(item);
       });
-      activeForm.after(box);
+      activeField.after(box);
     }
   );
 });
